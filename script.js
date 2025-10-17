56d50eaf027593f30d0608d40e78674ec7697fde
DevNovaMike <michaelgraham565@gmail.com>
2025-10-17 14:57:20 -0500

    Update Google Apps Script Web App URL

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     try {
       const response = await fetch(
-        // 👇 Replace this URL with your new "Appointment Messages" Google Apps Script Web App URL
+        https://script.google.com/macros/s/AKfycbzn2KOH4u6mnnJWfpyZk88SEQh3Kx1cAb_zg0E4QXdBzHk2D8FGQkvqcPN7JCVInTnW/exec
         "YOUR_NEW_APPOINTMENT_SCRIPT_URL_HERE",
         {
           method: "POST",
