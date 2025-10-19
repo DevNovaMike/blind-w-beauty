22091d26371a8cdda667d274146416530c546103
DevNovaMike <michaelgraham565@gmail.com>
2025-10-18 21:55:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     try {
       const response = await fetch(
-        "https://script.google.com/macros/s/AKfycbyZpLka1cEBztOGyMaCY3faIMcPaqIyi7lfWrb6W4cM1JsfiNijlJqiMZVu0I4wxgY8jg/exec",
+        "https://script.google.com/macros/s/AKfycbwzI4rqvk2MeshT1IkNOVOfB_5m6APDRumQwfTmqUNf8zcPsv5ek2lGlh6tjKDk9BDnmg/exec",
         {
           method: "POST",
           body: formData, // ✅ Supports file + text
